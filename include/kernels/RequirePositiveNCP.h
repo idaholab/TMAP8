ac92ad6f6b1a643612d89bffb374e405750008d3
Alex Lindsay <alexlindsay239@gmail.com>
2019-10-04 09:35:41 -0700

    Add test demonstrating non-singularity with a positively constrained boundary value problem PDE

---
+++ include/kernels/RequirePositiveNCP.h
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Kernel.h"
+
+// Forward Declarations
+class RequirePositiveNCP;
+
+template <>
+InputParameters validParams<RequirePositiveNCP>();
+
+class RequirePositiveNCP : public Kernel
+{
+public:
+  RequirePositiveNCP(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual() override;
+  virtual Real computeQpJacobian() override;
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
+
+private:
+  const unsigned int _v_var;
+  const VariableValue & _v;
+};
---
+++ src/kernels/RequirePositiveNCP.C
@@ -0,0 +1,52 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "RequirePositiveNCP.h"
+
+registerMooseObject("TMAPApp", RequirePositiveNCP);
+
+template <>
+InputParameters
+validParams<RequirePositiveNCP>()
+{
+  InputParameters params = validParams<Kernel>();
+  params.addRequiredCoupledVar("v", "The coupled variable we require to be non-negative");
+  return params;
+}
+
+RequirePositiveNCP::RequirePositiveNCP(const InputParameters & parameters)
+  : Kernel(parameters), _v_var(coupled("v")), _v(coupledValue("v"))
+{
+  if (_var.number() == _v_var)
+    mooseError("Coupled variable 'v' needs to be different from 'variable' with "
+               "RequirePositiveNCP");
+}
+
+Real
+RequirePositiveNCP::computeQpResidual()
+{
+  return _test[_i][_qp] * std::min(_u[_qp], _v[_qp]);
+}
+
+Real
+RequirePositiveNCP::computeQpJacobian()
+{
+  if (_u[_qp] <= _v[_qp])
+    return _test[_i][_qp] * _phi[_j][_qp];
+  return 0;
+}
+
+Real
+RequirePositiveNCP::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  if (jvar == _v_var)
+    if (_v[_qp] < _u[_qp])
+      return _test[_i][_qp] * _phi[_j][_qp];
+  return 0.0;
+}
