fcedc2d9b76f89719aa3c2350573acb8b4b59f37
Pierre-Clement Simon <pierreclement.simon@inl.gov>
2024-02-04 22:50:27 -0700

    - Update python script to plot results

--- test/tests/ver-1a/comparison_ver-1a.py
+++ test/tests/ver-1a/comparison_ver-1a.py
@@ -10,15 +10,15 @@ fig = plt.figure(figsize=[6.5,5.5])
 gs = gridspec.GridSpec(1,1)
 ax = fig.add_subplot(gs[0])
 
-tmap_sol = pd.read_csv("./ver-1a_csv.csv")
-analytical_sol = pd.read_csv("./analytical.csv")
-tmap_time = tmap_sol['time']
-tmap_fr = tmap_sol['rhs_release']
-analytical_time = analytical_sol['time(s)']
-analytical_fr = analytical_sol['frac_rel']
+tmap8_prediction = pd.read_csv("./ver-1a_csv.csv")
+analytical_solution = pd.read_csv("./analytical.csv")
+tmap_time = tmap8_prediction['time']
+tmap_release_fraction = tmap8_prediction['released_fraction']
+analytical_time = analytical_solution['time(s)']
+analytical_release_fraction = analytical_solution['frac_rel']
 
-ax.plot(tmap_time,tmap_fr,label=r"TMAP8",c='tab:gray')
-ax.plot(analytical_time,analytical_fr,label=r"Analytical",c='k', linestyle='--')
+ax.plot(tmap_time,tmap_release_fraction,label=r"TMAP8",c='tab:gray')
+ax.plot(analytical_time,analytical_release_fraction,label=r"Analytical",c='k', linestyle='--')
 
 
 ax.set_xlabel(u'Time(s)')
@@ -27,7 +27,7 @@ ax.legend(loc="best")
 ax.set_xlim(left=0)
 ax.set_xlim(right=45)
 ax.set_ylim(bottom=0)
-plt.grid(b=True, which='major', color='0.65', linestyle='--', alpha=0.3)
+plt.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
 
 ax.minorticks_on()
 plt.savefig('comparison.png', bbox_inches='tight');
