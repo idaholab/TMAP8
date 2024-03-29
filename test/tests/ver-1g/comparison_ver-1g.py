01fb73b1caae65c6eea6750de9644d51a05b6b44
ChaitanyaBhave-e <chaitanya.bhave@inl.gov>
2024-03-29 12:50:13 -0600

    Removed duplicated parts of input file and added tests for both TMAP4 and TMAP7 verification intial conditions ( #84 )

--- test/tests/ver-1g/comparison_ver-1g.py
+++ test/tests/ver-1g/comparison_ver-1g.py
@@ -12,23 +12,51 @@ ax = fig.add_subplot(gs[0])
 
 # ==============================================================================
 
-tmap_sol = pd.read_csv("./gold/ver-1g_diff_conc_out.csv")
+#TMAP4 case
+tmap_sol = pd.read_csv("./gold/diff_conc_TMAP4_out.csv")
 tmap_time = tmap_sol['time']
 tmap_concAB = tmap_sol['conc_ab']
-ax.plot(tmap_time, tmap_concAB, label=r"TMAP8", c='tab:gray')
+ax.plot(tmap_time, tmap_concAB, linewidth = 1.5, label=r"TMAP8 (TMAP4 case)", c='tab:brown')
 
-analytical_time = np.linspace(0.0, 40, 100)
-concA_o = 2.43e-4 # atoms / microns^3
-concB_o = 1.215e-4 # atoms / microns^3
-K = 4.14e3 # molecule.m^3/atom^2/s
+#TMAP7 case
+tmap_sol = pd.read_csv("./gold/diff_conc_TMAP7_out.csv")
+tmap_time = tmap_sol['time']
+tmap_concAB = tmap_sol['conc_ab']
+ax.plot(tmap_time, tmap_concAB, linewidth = 1.5, label=r"TMAP8 (TMAP7 case)", c='tab:gray')
 
-exponential_term = np.exp(K * analytical_time *
+def get_conc_from_pressure(P):
+    R = 8.314462                # Gas constant (https://physics.nist.gov/cgi-bin/cuu/Value?r)
+    T = 25+273.15               # Temperature (25 C -> K)
+    Na = 6.02214076E23          # Avogadro's constant (https://physics.nist.gov/cgi-bin/cuu/Value?na)
+    m3_to_microns3 = 1E18       # Convert m^3 to microns^3
+    # Using ideal gas law
+    return P*Na/(R*T*m3_to_microns3)
+
+def get_concAB_diff(analytical_time,P_A0,P_B0):
+    concA_o = get_conc_from_pressure(P_A0)  # atoms / microns^3
+    concB_o = get_conc_from_pressure(P_B0)  # atoms / microns^3
+    K = 4.14e3                              # molecule.m^3/atom^2/s
+    exponential_term = np.exp(K * analytical_time *
                           (concB_o - concA_o))
-analytical_concAB = concB_o * \
-    (1 - exponential_term) / (1 - (concB_o / concA_o) * exponential_term)
+    analytical_concAB = concB_o * \
+        (1 - exponential_term) / (1 - (concB_o / concA_o) * exponential_term)
+    return analytical_concAB
 
-ax.plot(analytical_time, analytical_concAB, 'k--',
-           label=r"Analytical")
+analytical_time = np.linspace(0.0, 40, 20)
+
+#TMAP4
+P_A0 = 1E-6                 # Pressure (Pa)
+P_B0 = 1E-7                 # Pressure (Pa)
+
+ax.plot(analytical_time, get_concAB_diff(analytical_time,P_A0,P_B0), 'b^',
+           label=r"Analytical TMAP4")
+
+#TMAP7
+P_A0 = 1E-6                 # Pressure (Pa)
+P_B0 = 5E-7                 # Pressure (Pa)
+
+ax.plot(analytical_time, get_concAB_diff(analytical_time,P_A0,P_B0), 'ko',
+           label=r"Analytical TMAP7")
 
 ax.set_xlabel(u'Time (seconds)')
 ax.set_ylabel(u"Concentration of AB (atoms / $\mu$m$^3$)")
@@ -48,18 +76,21 @@ fig = plt.figure(figsize=[6.5, 5.5])
 gs = gridspec.GridSpec(1, 1)
 ax = fig.add_subplot(gs[0])
 
-tmap_sol = pd.read_csv("./gold/ver-1g_same_conc_out.csv")
+tmap_sol = pd.read_csv("./gold/equal_conc_out.csv")
 tmap_time = tmap_sol['time']
 tmap_concAB = tmap_sol['conc_ab']
 ax.plot(tmap_time, tmap_concAB, label=r"TMAP8", c='tab:gray')
 
-analytical_time = np.linspace(0.0, 40, 100)
-concA_o = 2.43e-4 # atoms / microns^3 (which is equal to concB_o)
-K = 4.14e3 # molecule.m^3/atom^2/s
+def get_concAB_equal(analytical_time,P_A0):
+    concA_o = get_conc_from_pressure(P_A0)  # atoms / microns^3
+    K = 4.14e3                              # molecule.m^3/atom^2/s
+    analytical_concAB = concA_o - 1 / (1/concA_o + K*analytical_time)
+    return analytical_concAB
 
-analytical_concAB = concA_o - 1 / (1/concA_o + K*analytical_time)
+analytical_time = np.linspace(0.0, 40, 20)
+P_A0 = 1E-6                 # Pressure (Pa) (which is equal to P_B0)
 
-ax.plot(analytical_time, analytical_concAB, 'k--',
+ax.plot(analytical_time, get_concAB_equal(analytical_time,P_A0), 'ko',
            label=r"Analytical")
 
 ax.set_xlabel(u'Time (seconds)')
---
+++ test/tests/ver-1g/ver-1g.i
@@ -0,0 +1,129 @@
+## Run this file with one of three input files to simulate various verification cases
+## 1. equal_conc.i        -> TMAP4 and TMAP7 equal concentration case
+## 2. diff_conc_TMAP4.i  -> TMAP4 different concentration case
+## 3. diff_conc_TMAP7.i  -> TMAP7 different concentration case
+## Example: ~/projects/TMAP8/tmap8-opt -i ver-1g.i equal_conc.i
+
+R = 8.314462 # Gas constant (https://physics.nist.gov/cgi-bin/cuu/Value?r)
+T = '${units 25 degC -> K}' # Temperature
+Na = 6.02214076E23 # Avogadro's constant (https://physics.nist.gov/cgi-bin/cuu/Value?na)
+
+[Mesh]
+    type = GeneratedMesh
+    dim = 2
+[]
+
+[Variables]
+    [c_a]
+    []
+    [c_b]
+    []
+    [c_ab]
+        initial_condition = 0
+    []
+[]
+
+[Kernels]
+    [timeDerivative_c_a]
+        type = ADTimeDerivative
+        variable = c_a
+    []
+    [timeDerivative_c_b]
+        type = TimeDerivative
+        variable = c_b
+    []
+    [timeDerivative_c_ab]
+        type = TimeDerivative
+        variable = c_ab
+    []
+    [MatReaction_b]
+        type = ADMatReactionFlexible
+        variable = c_b
+        vs = 'c_a c_b'
+        coeff = -1
+        reaction_rate_name = K
+    []
+    [MatReaction_a]
+        type = ADMatReactionFlexible
+        variable = c_a
+        vs = 'c_b c_a'
+        coeff = -1
+        reaction_rate_name = K
+    []
+    [MatReaction_ab]
+        type = ADMatReactionFlexible
+        variable = c_ab
+        vs = 'c_b c_a'
+        coeff = 1
+        reaction_rate_name = K
+    []
+[]
+
+[Materials]
+    [K]
+        type = ADParsedMaterial
+        f_name = 'K'
+        function = '4.14e3' # units: molecule.micrometer^3/atom.second
+    []
+[]
+
+[BCs]
+    [c_a_neumann] # No flux on the sides
+        type = NeumannBC
+        variable = c_a
+        boundary = 'left right bottom top'
+        value = 0
+    []
+    [c_b_neumann] # No flux on the sides
+        type = NeumannBC
+        variable = c_b
+        boundary = 'left right bottom top'
+        value = 0
+    []
+    [c_ab_neumann] # No flux on the sides
+        type = NeumannBC
+        variable = c_ab
+        boundary = 'left right bottom top'
+        value = 0
+    []
+[]
+
+[Postprocessors]
+    [conc_a]
+        type = ElementAverageValue
+        variable = c_a
+    []
+    [conc_b]
+        type = ElementAverageValue
+        variable = c_b
+    []
+    [conc_ab]
+        type = ElementAverageValue
+        variable = c_ab
+    []
+[]
+
+[Executioner]
+    type = Transient
+    scheme = bdf2
+    nl_rel_tol = 1e-10
+    nl_abs_tol = 1e-15
+
+    solve_type = 'NEWTON'
+
+    petsc_options = '-snes_ksp_ew'
+    petsc_options_iname = '-pc_type'
+    petsc_options_value = 'lu'
+
+    start_time = 0.0
+    end_time = 40
+    num_steps = 60000
+    dt = .2
+    n_startup_steps = 0
+[]
+
+[Outputs]
+    exodus = true
+    csv = true
+[]
+  
