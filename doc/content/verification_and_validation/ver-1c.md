7550bb8a03ef81e1f79de2418bbad7f5b022c818
Chaitanya Bhave <65254651+chaibhave@users.noreply.github.com>
2024-04-03 19:22:33 -0600

    Apply suggestions from code review
    Co-authored-by: Pierre-Clement Simon <pierreclement.simon@gmail.com>

--- doc/content/verification_and_validation/ver-1c.md
+++ doc/content/verification_and_validation/ver-1c.md
@@ -2,8 +2,8 @@
 
 # Diffusion Problem with Partially Preloaded Slab
 
-This verification problem is taken from [!cite](ambrosek2008verification,longhurst1992verification). Diffusion of tritium through a semi-infinite SiC layer is modeled with an initial
-loading of 1 atom/m{^3} in the first 10 m of a 2275-m slab. Diffusivity is unity
+This verification problem is taken from [!cite](longhurst1992verification,ambrosek2008verification). Diffusion of tritium through a semi-infinite SiC layer is modeled with an initial
+loading of 1 atom/m$^3$ in the first 10 m of a 2275 m slab. Diffusivity is set to 1 m$^2$/s
 and no traping is included. The analytical solution is given by:
 
 \begin{equation}
@@ -12,25 +12,25 @@ C = \frac{C_0}{2} \left[ \text{erf}\bigg(\frac{h-x}{2\sqrt{Dt}}\bigg) +
 \text{erf}\bigg(\frac{h+x}{2\sqrt{Dt}}\bigg)  \right]
 \end{equation}
 
-where h is the thickness of the pre-loaded portion of the layer.
+where $h=10$ m is the thickness of the pre-loaded portion of the layer.
 
-!alert warning title=Typo in [!cite](ambrosek2008verification,longhurst1992verification)
-[eq:c_func] for the value of $C$ is obtained from [!cite](ambrosek2008verification), but the equation there has a typographical error and gives incorrect results. The equation in [!cite](longhurst1992verification) also has typographical errors, and adds an extra term to the equation that gives incorrect results.
+!alert warning title=Typo in [!cite](longhurst1992verification,)
+[eq:c_func] for the value of $C$ is based on the expression from [!cite](longhurst1992verification), but the equation in [!cite](longhurst1992verification) has a typographical error and gives incorrect results ($\sqrt(Dt)$ should be at the denominator). The equation in [!cite](ambrosek2008verification) also has typographical errors, and adds an extra term to the equation that gives incorrect results.
 
-TMAP4 performs comparison of the mobile species concentration at three points - (a) a point at the interface (x = 0 m), (b) a point at the end of the pre-loaded region (x = 10 m), and (c) a point in the initally unloaded region (x = 12 m). The comparison of the values calculated through TMAP8 and analytically for the TMAP4 case is shown in
-[ver-1c_comparison_time_TMAP4]. TMAP7 performs comparison of the mobile species concentration at the same points (b) and (c) as in the TMAP4 case, but performs the third comparison at (d) a point close to the interface (x = 0.25 m). The comparison of the values calculated through TMAP8 and analytically for the TMAP7 case is shown in
-[ver-1c_comparison_time_TMAP7]. The TMAP8 calculations are found to be in good agreement with the analytical solution.
+TMAP4 and TMAP7 verification cases are slightly different: TMAP4 verifies the mobile species concentration at three points - (a) a point at the free surface (x = 0 m), (b) a point at the end of the pre-loaded region (x = 10 m), and (c) a point in the initially unloaded region (x = 12 m). The comparison of the values calculated with TMAP8 and analytically for the TMAP4 cases is shown in
+[ver-1c_comparison_time_TMAP4]. TMAP7 verifies the mobile species concentration at the same points (b) and (c) as in the TMAP4 case, but performs the third comparison at (d) a point close to the free surface (x = 0.25 m). The comparison of the values calculated with TMAP8 and analytically for the TMAP7 cases is shown in
+[ver-1c_comparison_time_TMAP7]. In all cases, the TMAP8 calculations are found to be in good agreement with the analytical solution.
 
 !media figures/ver-1c_comparison_time_TMAP4.png
     style=width:50%;margin-bottom:2%
     id=ver-1c_comparison_time_TMAP4
-    caption=Comparison of concentration as function of time at x\=0 m, 10 m and 12 m
-    calculated through TMAP8 and analytically (TMAP4 case)
+    caption=Comparison of concentration as a function of time at x\=0 m, 10 m, and 12 m
+    calculated with TMAP8 and analytically (TMAP4 cases)
 
 !media figures/ver-1c_comparison_time_TMAP7.png
     style=width:50%;margin-bottom:2%
     id=ver-1c_comparison_time_TMAP7
-    caption=Comparison of concentration as function of time at x\=0.25 m, 10 m and 12 m
-    calculated through TMAP8 and analytically (TMAP7 case)
+    caption=Comparison of concentration as a function of time at x\=0.25 m, 10 m, and 12 m
+    calculated with TMAP8 and analytically (TMAP7 cases)
 
 !bibtex bibliography
--- test/tests/ver-1c/comparison_ver-1c.py
+++ test/tests/ver-1c/comparison_ver-1c.py
@@ -6,17 +6,17 @@ import pandas as pd
 from scipy.special import erf
 from numpy import sqrt
 
-tmap_sol = pd.read_csv("ver-1c_csv.csv")
+tmap_sol = pd.read_csv("./gold/ver-1c_csv.csv")
 tmap_time = tmap_sol['time'][1:]
 tmap_conc0 = tmap_sol['point0'][1:]
 tmap_conc0_25 = tmap_sol['point0.25'][1:]
 tmap_conc10 = tmap_sol['point10'][1:]
 tmap_conc12 = tmap_sol['point12'][1:]
 
-def get_c_over_time_TMAP4(x,t):
-    c0 = 1
-    h = 10
-    D = 1
+def get_c_analytical(x,t):
+    c0 = 1 # atom/m^3
+    h = 10 # m
+    D = 1  # m^2/s
 
     erf_func1 = erf((h-x)/(2*sqrt(D*t)) )
     erf_func2 = erf((h+x)/(2*sqrt(D*t)) )
@@ -24,10 +24,10 @@ def get_c_over_time_TMAP4(x,t):
 
 idx = np.where(tmap_time >= 10.0)[0][0]
 
-analytical_conc0 = get_c_over_time_TMAP4(0,tmap_time)
-analytical_conc0_25 = get_c_over_time_TMAP4(0.25,tmap_time)
-analytical_conc10 = get_c_over_time_TMAP4(x=10,t=tmap_time)
-analytical_conc12 = get_c_over_time_TMAP4(x=12,t=tmap_time)
+analytical_conc0 = get_c_analytical(0,tmap_time)
+analytical_conc0_25 = get_c_analytical(0.25,tmap_time)
+analytical_conc10 = get_c_analytical(x=10,t=tmap_time)
+analytical_conc12 = get_c_analytical(x=12,t=tmap_time)
 
 
 #========= Comparison of concentration as a function of time (TMAP4 case) ===================
@@ -36,20 +36,20 @@ fig = plt.figure(figsize=[6.5,5.5])
 gs = gridspec.GridSpec(1,1)
 ax = fig.add_subplot(gs[0])
 
-ax.plot(tmap_time,tmap_conc0,label=r"TMAP8 x=0",c='tab:blue')
-ax.plot(tmap_time,analytical_conc0,label=r"Analytical TMAP7 x=0",c='tab:brown', linestyle='--', dashes=(5,5))
+ax.plot(tmap_time,tmap_conc0,label=r"TMAP8 x=0 m",c='tab:blue')
+ax.plot(tmap_time,analytical_conc0,label=r"Analytical x=0 m",c='tab:brown', linestyle='--', dashes=(5,5))
 RMSE = np.sqrt(np.mean((tmap_conc0-analytical_conc0)[idx:]**2) )
 RMSPE = RMSE*100/np.mean(analytical_conc0[idx:])
 ax.text(20,0.9, 'RMSPE = %.2f '%RMSPE+'%',fontweight='bold')
 
-ax.plot(tmap_time,tmap_conc10,label=r"TMAP8 x=10",c='tab:red')
-ax.plot(tmap_time,analytical_conc10,label=r"Analytical x=10",c='tab:cyan', linestyle='--', dashes=(5,5))
+ax.plot(tmap_time,tmap_conc10,label=r"TMAP8 x=10 m",c='tab:red')
+ax.plot(tmap_time,analytical_conc10,label=r"Analytical x=10 m",c='tab:cyan', linestyle='--', dashes=(5,5))
 RMSE = np.sqrt(np.mean((tmap_conc10-analytical_conc10)[idx:]**2) )
 RMSPE = RMSE*100/np.mean(analytical_conc10[idx:])
 ax.text(20,0.55, 'RMSPE = %.2f '%RMSPE+'%',fontweight='bold')
 
-ax.plot(tmap_time,tmap_conc12,label=r"TMAP8 x=12",c='tab:grey')
-ax.plot(tmap_time,analytical_conc12,label=r"Analytical x=12",c='k', linestyle='--', dashes=(5,5))
+ax.plot(tmap_time,tmap_conc12,label=r"TMAP8 x=12 m",c='tab:grey')
+ax.plot(tmap_time,analytical_conc12,label=r"Analytical x=12 m",c='k', linestyle='--', dashes=(5,5))
 RMSE = np.sqrt(np.mean((tmap_conc12-analytical_conc12)[idx:]**2) )
 RMSPE = RMSE*100/np.mean(analytical_conc12[idx:])
 ax.text(20,0.25, 'RMSPE = %.2f '%RMSPE+'%',fontweight='bold')
@@ -72,20 +72,20 @@ fig = plt.figure(figsize=[6.5,5.5])
 gs = gridspec.GridSpec(1,1)
 ax = fig.add_subplot(gs[0])
 
-ax.plot(tmap_time,tmap_conc0_25,label=r"TMAP8 x=0.25",c='tab:blue')
-ax.plot(tmap_time,analytical_conc0_25,label=r"Analytical TMAP7 x=0",c='tab:brown', linestyle='--', dashes=(5,5))
+ax.plot(tmap_time,tmap_conc0_25,label=r"TMAP8 x=0.25 m",c='tab:blue')
+ax.plot(tmap_time,analytical_conc0_25,label=r"Analytical x=0.25 m",c='tab:brown', linestyle='--', dashes=(5,5))
 RMSE = np.sqrt(np.mean((tmap_conc0_25-analytical_conc0_25)[idx:]**2) )
 RMSPE = RMSE*100/np.mean(analytical_conc0_25[idx:])
 ax.text(20,0.9, 'RMSPE = %.2f '%RMSPE+'%',fontweight='bold')
 
-ax.plot(tmap_time,tmap_conc10,label=r"TMAP8 x=10",c='tab:red')
-ax.plot(tmap_time,analytical_conc10,label=r"Analytical x=10",c='tab:cyan', linestyle='--', dashes=(5,5))
+ax.plot(tmap_time,tmap_conc10,label=r"TMAP8 x=10 m",c='tab:red')
+ax.plot(tmap_time,analytical_conc10,label=r"Analytical x=10 m",c='tab:cyan', linestyle='--', dashes=(5,5))
 RMSE = np.sqrt(np.mean((tmap_conc10-analytical_conc10)[idx:]**2) )
 RMSPE = RMSE*100/np.mean(analytical_conc10[idx:])
 ax.text(20,0.55, 'RMSPE = %.2f '%RMSPE+'%',fontweight='bold')
 
-ax.plot(tmap_time,tmap_conc12,label=r"TMAP8 x=12",c='tab:grey')
-ax.plot(tmap_time,analytical_conc12,label=r"Analytical x=12",c='k', linestyle='--', dashes=(5,5))
+ax.plot(tmap_time,tmap_conc12,label=r"TMAP8 x=12 m",c='tab:grey')
+ax.plot(tmap_time,analytical_conc12,label=r"Analytical x=12 m",c='k', linestyle='--', dashes=(5,5))
 RMSE = np.sqrt(np.mean((tmap_conc12-analytical_conc12)[idx:]**2) )
 RMSPE = RMSE*100/np.mean(analytical_conc12[idx:])
 ax.text(20,0.25, 'RMSPE = %.2f '%RMSPE+'%',fontweight='bold')
