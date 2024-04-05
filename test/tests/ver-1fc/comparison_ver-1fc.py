a73d079640c40332447836f52797c69f71fce9be
Pierre-Clement Simon <pierreclement.simon@inl.gov>
2024-04-04 19:28:32 -0600

    Add tests

--- test/tests/ver-1fc/comparison_ver-1fc.py
+++ test/tests/ver-1fc/comparison_ver-1fc.py
@@ -4,69 +4,125 @@ import numpy as np
 from matplotlib import gridspec
 import pandas as pd
 from scipy import special
+import os
 
+# Changes working directory to script directory (for consistent MooseDocs usage)
+os.chdir(os.path.dirname(__file__))
+
+#===============================================================================
+# Extract TMAP8 results (both steady-state and transient)
+tmap8_sol_steady_state = pd.read_csv("./gold/ver-1fc_vector_postproc_line_0105.csv")
+tmap8_sol_steady_state_x = tmap8_sol_steady_state['id']
+tmap8_sol_steady_state_temperature = tmap8_sol_steady_state['temperature']
+
+tmap8_sol_transient = pd.read_csv("./gold/ver-1fc_vector_postproc_line_0053.csv")
+tmap8_sol_transient_x = tmap8_sol_transient['id']
+tmap8_sol_transient_temperature = tmap8_sol_transient['temperature']
+
+tmap8_sol_transient = pd.read_csv("./gold/ver-1fc_temperature_at_x0.09.csv")
+tmap8_sol_transient_t = tmap8_sol_transient['time']
+tmap8_sol_transient_temperature_at_x = tmap8_sol_transient['temperature_at_x']
+
+
+#===============================================================================
+# Extract ABAQUS results (for transient only since the steady-state results were not provided)
+abaqus_sol_transient = pd.read_csv("./ver-1fc_abaqus_TMAP7_results_over_distance.csv")
+abaqus_sol_transient_x = abaqus_sol_transient['Distance']
+abaqus_sol_transient_temperature_1 = abaqus_sol_transient['abaqus_or_TMAP7_temperature_transient_1']
+abaqus_sol_transient_temperature_2 = abaqus_sol_transient['abaqus_or_TMAP7_temperature_transient_2']
+
+abaqus_sol_transient = pd.read_csv("./ver-1fc_abaqus_TMAP7_results_over_time.csv")
+abaqus_sol_transient_t = abaqus_sol_transient['time']
+abaqus_sol_transient_temperature_at_x = abaqus_sol_transient['abaqus_temperature_transient']
+tmap7_sol_transient_temperature_at_x = abaqus_sol_transient['TMAP7_temperature_transient']
+
+#===============================================================================
+# Calculate analytical solution (steady-state)
+
+def get_analytical_solution_steady_state(x):
+    """Returns the temperature as a function of the distance to the left side of the domain
+
+    Args:
+        x (ndarray): distance in m
+
+    Returns:
+        np.array: temperature in K at position x
+    """
+    T_SA = 600 # K
+    T_SB = 0 # K
+    L_A = 40e-2 # m
+    L_B = 40e-2 # m
+    k_A = 401 # W/m/K
+    k_B = 80.2 # W/m/K
+    # calculate interface temperature
+    T_I = (T_SA*k_A/L_A + T_SB*k_B/L_B)/(k_A/L_A + k_B/L_B)
+
+    # determine temperature at position x by linear interpolation (steady-state)
+    temperature = [None]*len(x)
+    for i in range(len(x)):
+        if x[i]<=L_A:
+            temperature[i] = (T_I-T_SA)/L_A*x[i] + T_SA
+        else:
+            temperature[i] = (T_SB-T_I)/L_B*(x[i]-L_A-L_B) + T_SB
+
+    return temperature
+
+#===============================================================================
+# Plot temperature profile for steady-state
 
 fig = plt.figure(figsize=[6.5, 5.5])
 gs = gridspec.GridSpec(1, 1)
 ax = fig.add_subplot(gs[0])
-
-num_summation_terms = 10
-
-
-def summation_terms(n, x, t, alph):
-    sum = 0.0
-    for m in range(1, n):
-        lambdaa = m * np.pi / L
-        sum += np.sin(lambdaa * x) * np.exp(-1 *
-                                            alph * lambdaa**2 * t) / lambdaa
-    return sum
-
-
-analytical_x = np.linspace(0.0, 4.0, 40)
-To = 300
-T1 = 400
-alpha = 1.0
-L = 4.0
-time = [0.1, 0.5, 1.0, 5.0]
-
-# analytical_temp = []
-# for i in range(len(time)):
-#     analytical_temp.append(To + (T1-To) * (1 - (analytical_x/L) - (2/L) *
-#                            summation_terms(num_summation_terms, analytical_x, time[i], alpha)))
-
-# ax.scatter(analytical_x,
-#            analytical_temp[0], label=r"Analytical 0.1 seconds", c='k', marker='^')
-# ax.scatter(analytical_x,
-#            analytical_temp[1], label=r"Analytical 0.5 seconds", c='r', marker='^')
-# ax.scatter(analytical_x,
-#            analytical_temp[2], label=r"Analytical 1.0 seconds", c='b', marker='^')
-# ax.scatter(analytical_x,
-#            analytical_temp[3], label=r"Analytical 5.0 seconds", c='c', marker='^')
-
-tmap_temp = []
-tmap_sol = pd.read_csv("./ver-1fc_out_line_1500.csv")
-tmap_x = tmap_sol['id']
-tmap_temp.append(tmap_sol['temp'])
-# tmap_sol = pd.read_csv("./ver-1fc_out_line_0015.csv")
-# tmap_temp.append(tmap_sol['temp'])
-# tmap_sol = pd.read_csv("./ver-1fc_out_line_0100.csv")
-# tmap_temp.append(tmap_sol['temp'])
-# tmap_sol = pd.read_csv("./ver-1fc_out_line_1500.csv")
-# tmap_temp.append(tmap_sol['temp'])
-
-ax.plot(tmap_x, tmap_temp[0], label=r"TMAP8 150 seconds", c='k')
-# ax.plot(tmap_x, tmap_temp[1], label=r"TMAP8 150 seconds", c='r')
-# ax.plot(tmap_x, tmap_temp[2], label=r"TMAP8 1000 seconds", c='b')
-# ax.plot(tmap_x, tmap_temp[3], label=r"TMAP8 15000 seconds", c='c')
-
-
-ax.set_xlabel(u'Distance along slab (m)')
-ax.set_ylabel(u"Temperature (K)")
+ax.plot(tmap8_sol_steady_state_x,tmap8_sol_steady_state_temperature,label=r"TMAP8",c='tab:gray')
+ax.plot(tmap8_sol_steady_state_x,get_analytical_solution_steady_state(tmap8_sol_steady_state_x),label=r"Analytical",c='k', linestyle='--')
+ax.set_xlabel(u'Distance (m)')
+ax.set_ylabel(r"Temperature (K)")
 ax.legend(loc="best")
-# ax.set_xlim(left=0)
-# ax.set_ylim(bottom=300)
-plt.grid(visible=True, which='major', color='0.65', linestyle='--', alpha=0.3)
-
+ax.set_xlim(left=0)
+ax.set_xlim(right=max(tmap8_sol_steady_state_x))
+ax.set_ylim(bottom=0)
+plt.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
 ax.minorticks_on()
-plt.savefig('ver-1fc_comparison_temperature.png', bbox_inches='tight')
+plt.savefig('ver-1fc_comparison_temperature_steady_state.png', bbox_inches='tight');
 plt.close(fig)
+
+#===============================================================================
+# Plot temperature profile as a function of distance for transient (at t = 150 s)
+
+fig = plt.figure(figsize=[6.5, 5.5])
+gs = gridspec.GridSpec(1, 1)
+ax = fig.add_subplot(gs[0])
+ax.plot(tmap8_sol_transient_x,tmap8_sol_transient_temperature,label=r"TMAP8",c='tab:gray')
+ax.plot(abaqus_sol_transient_x,abaqus_sol_transient_temperature_1,label=r"ABAQUS or TMAP7 (1)",c='b', linestyle='--')
+ax.plot(abaqus_sol_transient_x,abaqus_sol_transient_temperature_2,label=r"ABAQUS or TMAP7 (2)",c='r', linestyle='-.')
+ax.set_xlabel(u'Distance (m)')
+ax.set_ylabel(r"Temperature (K)")
+ax.legend(loc="best")
+ax.set_xlim(left=0)
+ax.set_xlim(right=max(tmap8_sol_transient_x))
+ax.set_ylim(bottom=0)
+plt.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
+ax.minorticks_on()
+plt.savefig('ver-1fc_comparison_temperature_transient_t150.png', bbox_inches='tight');
+plt.close(fig)
+
+#===============================================================================
+# Plot temperature evolution as a function of time for transient (at x = 0.09 m)
+
+fig = plt.figure(figsize=[6.5, 5.5])
+gs = gridspec.GridSpec(1, 1)
+ax = fig.add_subplot(gs[0])
+ax.plot(tmap8_sol_transient_t,tmap8_sol_transient_temperature_at_x,label=r"TMAP8",c='tab:gray')
+ax.plot(abaqus_sol_transient_t,abaqus_sol_transient_temperature_at_x,label=r"ABAQUS)",c='b', linestyle='--')
+ax.plot(abaqus_sol_transient_t,tmap7_sol_transient_temperature_at_x,label=r"TMAP7",c='r', linestyle='-.')
+ax.set_xlabel(u'Distance (m)')
+ax.set_ylabel(r"Temperature (K)")
+ax.legend(loc="best")
+ax.set_xlim(left=0)
+ax.set_xlim(right=max(tmap8_sol_transient_t)/10)
+ax.set_ylim(bottom=0)
+plt.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
+ax.minorticks_on()
+plt.savefig('ver-1fc_comparison_temperature_transient_x0.09.png', bbox_inches='tight');
+plt.close(fig)
+
