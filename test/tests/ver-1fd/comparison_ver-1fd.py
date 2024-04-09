906dd89e05b334ba35a63a0da2f5f4c4719e8171
Pierre-Clement Simon <pierreclement.simon@inl.gov>
2024-04-08 18:19:37 -0600

    Add tests, gold files, and python script

--- test/tests/ver-1fd/comparison_ver-1fd.py
+++ test/tests/ver-1fd/comparison_ver-1fd.py
@@ -4,42 +4,56 @@ import numpy as np
 from matplotlib import gridspec
 import pandas as pd
 from scipy import special
+import os
+import math
 
+# Changes working directory to script directory (for consistent MooseDocs usage)
+os.chdir(os.path.dirname(__file__))
 
-# ==============================================================================
+#===============================================================================
+# Extract TMAP8 results
+tmap8_sol = pd.read_csv("./gold/ver-1fd_out.csv")
+tmap8_sol_t = tmap8_sol['time']
+tmap8_sol_temperature = tmap8_sol['temperature_at_x']
+
+#===============================================================================
+# Calculate analytical solution
+def get_analytical_solution(x,t_vect):
+    """Returns the temperature as a function of the distance to the left side of the domain and time
+
+    Args:
+        x (float): distance in m
+        t (ndarray): time in s
+
+    Returns:
+        np.array: temperature in K at position x
+    """
+    T_initial = 100 # K
+    T_infinity = 500 # K
+    h = 200 # W/m^2/K)
+    k = 401 # W/m/K
+    rho_Cp = 3.439e6 # J/m^3/K
+    alpha = k/rho_Cp # ~1.17e-4 m^2/s
+    small_value = 1e-42 # to avoid division by 0
+
+    temperature = [T_initial + (T_infinity - T_initial) * (math.erfc( x/(2 * math.sqrt(t * alpha) + small_value)) -  math.exp(h*x/k + h*h*t*alpha/k/k)*math.erfc( x/(2 * math.sqrt(t * alpha) + small_value)  + h*math.sqrt(t * alpha)/k)) for t in t_vect]
+    return temperature
+
+#===============================================================================
+# Plot temperature evolution as a function of time (at x = 0.05 m)
+
 fig = plt.figure(figsize=[6.5, 5.5])
 gs = gridspec.GridSpec(1, 1)
 ax = fig.add_subplot(gs[0])
-
-# analytical_time = np.linspace(0.0, 40, 40)
-# concA_o = 2.43e-4
-# concB_o = 2.43001e-4  # Added 0.00001e-4 to actual value to avoid divide-by-zero
-# # error in the analytical solution
-# concAB_o = 0
-# K = 4.14e3
-
-# exponential_term = np.exp(K * analytical_time *
-#                           (concB_o - concA_o))
-# analytical_concAB = concB_o * \
-#     (1 - exponential_term) / (1 - (concB_o / concA_o) * exponential_term)
-
-# ax.scatter(analytical_time, analytical_concAB,
-#            label=r"Analytical", c='k', marker='^')
-
-tmap_sol = pd.read_csv("./ver-1fd_out.csv")
-tmap_time = tmap_sol['time']
-tmap_temp = tmap_sol['5cm_node_temp']
-ax.plot(tmap_time, tmap_temp, label=r"TMAP8", c='tab:blue')
-
-ax.set_xlabel(u'Time (seconds)')
-ax.set_ylabel(u"Temperature (K)")
+ax.plot(tmap8_sol_t,tmap8_sol_temperature,label=r"TMAP8",c='tab:gray')
+ax.plot(tmap8_sol_t,get_analytical_solution(5e-2,tmap8_sol_t),label=r"Analytical",c='k', linestyle='--')
+ax.set_xlabel(u'Time (s)')
+ax.set_ylabel(r"Temperature (K)")
 ax.legend(loc="best")
 ax.set_xlim(left=0)
+ax.set_xlim(right=max(tmap8_sol_t))
 ax.set_ylim(bottom=100)
-plt.grid(visible=True, which='major',
-         color='0.65', linestyle='--', alpha=0.3)
-
+plt.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
 ax.minorticks_on()
-plt.savefig('ver-1fd_comparison.png', bbox_inches='tight')
-
+plt.savefig('ver-1fd_comparison_convective_heating.png', bbox_inches='tight');
 plt.close(fig)
