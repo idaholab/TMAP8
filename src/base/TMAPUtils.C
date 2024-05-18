394bfb69b2ff15db4bff8df976ac3f674828ea2f
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-05-17 19:26:01 -0600

    Adapt shared parameters to definition of components

--- src/base/TMAPUtils.C
+++ src/base/TMAPUtils.C
@@ -24,11 +24,6 @@ commonParams()
   params.addParam<std::vector<Real>>(
       "species_scaling_factors",
       "Scaling factors to make the (non)linear system better conditioned");
-  params.addParam<Real>(
-      "length_unit",
-      1,
-      "The number of length units in a meter. This allows the user to select length units "
-      "other than meters that may lead to better overall scaling of the system.");
   return params;
 }
 }
@@ -39,8 +34,8 @@ structureCommonParams()
   auto params = internal::commonParams();
   params.addParam<std::vector<SubdomainName>>(
       "block", "The list of block ids (SubdomainID) that this object will be applied");
-  params.addParam<std::vector<Real>>("species_initial_concentrations",
-                                     "Initial concentrations for the structure species");
+  params.addParam<std::vector<Real>>(
+      "species_initial_concentrations", {}, "Initial concentrations for the structure species");
   params.addRequiredParam<std::vector<FunctionName>>("diffusivities",
                                                      "The diffusivities of the species");
   return params;
@@ -50,20 +45,29 @@ InputParameters
 enclosureCommonParams()
 {
   auto params = internal::commonParams();
+
   // params.addRequiredParam<std::string>("material", "Fluid material name");
-  params.addRequiredParam<Real>("volume", "Volume of enclosure [m^3]");
   params.addRequiredParam<Real>("temperature", "Enclosure temperature [K]");
-  // params.addRequiredParam<FunctionName>("pressure", "Enclosure pressure [Pa]");
-  params.addParam<FunctionName>(
-      "D_h", "Hydraulic diameter [m]"); // required if mass transfer correlation is to be used
+  params.addRequiredParam<Real>("volume", "Volume of enclosure [m^3]");
+  params.addRequiredParam<Real>("surface_area", "Contact surface with the structure [m^3]");
+  params.addRequiredParam<ComponentName>("connected_structure",
+                                         "Structure exchanging species with the enclosure");
+  params.addRequiredParam<BoundaryName>(
+      "boundary",
+      "Surface between the enclosure and the connected structure. This surface should exist on the "
+      "mesh and likely be located on the 'connected_structure'");
+  // required if mass transfer correlation is to be used
+  // TODO: decide if it unstructured_mesh
+  // params.addParam<FunctionName>("D_h", "Hydraulic diameter [m]");
+
   params.addParam<std::vector<Real>>(
       "species_initial_pressures",
+      {},
       "Initial partial pressures for the enclosure species in Pascals");
-  params.addParam<Real>(
-      "pressure_unit",
-      1,
-      "The number of pressure units in a Pascal. This allows the user to select pressure units "
-      "other than Pascals that may lead to better overall scaling of the system.");
+
+  params.addParamNamesToGroup("species_initial_pressures", "Species");
+  params.addParamNamesToGroup("temperature", "Enclosure conditions");
+  params.addParamNamesToGroup("volume surface_area connected_structure boundary", "Geometry");
   return params;
 }
 }
