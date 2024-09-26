9cd59f6a2b9bcf359a22ecb958da9ee259edf55b
Lin Yang <132409718+lin-yang-ly@users.noreply.github.com>
2024-09-26 14:28:29 -0600

    Update git issue on Apptainer

--- test/tests/ver-1dd/comparison_ver-1dd.py
+++ test/tests/ver-1dd/comparison_ver-1dd.py
@@ -3,10 +3,10 @@ import numpy as np
 from matplotlib import gridspec
 import pandas as pd
 import os
-import git
 
 # Changes working directory to script directory (for consistent MooseDocs usage)
-os.chdir(os.path.dirname(__file__))
+script_folder = os.path.dirname(__file__)
+os.chdir(script_folder)
 
 num_summation_terms = 1000
 N_o = 3.1622e22             # For convenience lattice density chosen as 3.1622e22 atom/m^3
@@ -15,7 +15,11 @@ D = 1                       # diffusivity (m^2/s)
 l = 1                       # slab thickness (m)
 
 # Extract data from 'gold' TMAP8 run
-tmap_sol = pd.read_csv(os.path.join(git.Repo('.',search_parent_directories=True).working_tree_dir, "test/tests/ver-1dd/gold/ver-1dd_out.csv"))
+if "doc" in script_folder.split("/"):  # if in documentation folder
+    csv_folder = "../../../../test/tests/ver-1dd/gold/ver-1dd_out.csv"
+else:                                   # if in test folder
+    csv_folder = "./gold/ver-1dd_out.csv"
+tmap_sol = pd.read_csv(csv_folder)
 tmap_time = np.array(tmap_sol['time'])
 tmap_prediction = np.array(tmap_sol['scaled_outflux'])
 idx = np.where(tmap_time >= 0.01)[0][0]
