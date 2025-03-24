2c924930e60e188258879d733b09d23d17ea69d5
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-03-24 07:18:14 -0600

    Allow vectors of reacting species and vectors of product species: this gives 'full' descriptive capabilities even though TMAP8 realistically 99% of the time always has H/H2/H3 reacting with a single other species

--- src/physics/SpeciesDiffusionReactionCG.C
+++ src/physics/SpeciesDiffusionReactionCG.C
@@ -19,20 +19,22 @@ InputParameters
 SpeciesDiffusionReactionCG::validParams()
 {
   InputParameters params = MultiSpeciesDiffusionCG::validParams();
-  params.addClassDescription(
-      "Discretizes a diffusion equation with the continuous Galerkin finite element method");
+  params.addClassDescription("Discretizes a diffusion equation with one or more reaction terms "
+                             "with the continuous Galerkin finite element method");
 
   // Add reaction parameters
   // Until we support a Component-based input, these are required in the Physics parameters
   params.addRequiredParam<std::vector<std::vector<VariableName>>>(
-      "reacting_species", "For each species (outer indexing), the list of species they react with");
+      "reacting_species",
+      "The groups of reacting species that react. The "
+      "outer indexing separates each group, while the inner indexing is used to "
+      "describe the groups");
   params.addParam<std::vector<std::vector<VariableName>>>(
       "product_species",
       {},
-      "For each species (outer indexing), for each reactant, the species being created");
-  params.addRequiredParam<std::vector<std::vector<MaterialPropertyName>>>(
-      "reaction_coefficients",
-      "For each species (outer indexing), the reaction coefficient for the reaction");
+      "For each (group of) reactant, the species (innermost indexing) being created");
+  params.addRequiredParam<std::vector<MaterialPropertyName>>(
+      "reaction_coefficients", "The reaction coefficient for each reaction");
   params.addParamNamesToGroup("reacting_species product_species reaction_coefficients",
                               "Reaction Network");
 
@@ -42,11 +44,13 @@ SpeciesDiffusionReactionCG::validParams()
 SpeciesDiffusionReactionCG::SpeciesDiffusionReactionCG(const InputParameters & parameters)
   : MultiSpeciesDiffusionCG(parameters)
 {
-  checkTwoDVectorParamsSameLength<VariableName, MaterialPropertyName>("reacting_species",
-                                                                      "reaction_coefficients");
+  // There must be as many groups of reactants as coefficients for the reactions
+  checkVectorParamsSameLength<std::vector<VariableName>, MaterialPropertyName>(
+      "reacting_species", "reaction_coefficients");
+  // There must be as many groups of reactants as groups of products, if any product
   if (isParamSetByUser("product_species"))
-    checkTwoDVectorParamsSameLength<VariableName, VariableName>("reacting_species",
-                                                                "product_species");
+    checkVectorParamsSameLength<std::vector<VariableName>, std::vector<VariableName>>(
+        "reacting_species", "product_species");
 }
 
 void
@@ -61,62 +65,73 @@ SpeciesDiffusionReactionCG::addFEKernels()
   const auto & product_species =
       getParam<std::vector<std::vector<VariableName>>>("product_species");
   const auto & reaction_coeffs =
-      getParam<std::vector<std::vector<MaterialPropertyName>>>("reaction_coefficients");
+      getParam<std::vector<MaterialPropertyName>>("reaction_coefficients");
 
-  for (const auto s : index_range(_species_names))
+  // Reaction term
+  if (isParamValid("reacting_species"))
   {
-    const auto & var_name = _species_names[s];
-    // Reaction term
-    if (isParamValid("reacting_species"))
-    {
-      // Add a kernel for both directions
-      std::string kernel_type = "ADMatReactionFlexible";
-      InputParameters params = getFactory().getValidParams(kernel_type);
-      params.set<NonlinearVariableName>("variable") = var_name;
-      assignBlocks(params, _blocks);
-      // Note: since we do not support per-component input of reacting species at this time,
-      // we use the Physics' block restriction rather than the Component's
+    // Add a kernel for both directions
+    std::string kernel_type = "ADMatReactionFlexible";
+    InputParameters params = getFactory().getValidParams(kernel_type);
+    assignBlocks(params, _blocks);
+    // Note: since we do not support per-component input of reacting species at this time,
+    // we use the Physics' block restriction rather than the Component's
 
-      // Double-indexed vectors arent initialized as I expected
-      if (reacting_species.size() <= s)
+    // Loop on reaction equations (= groups of reactants and products)
+    for (const auto c : index_range(reacting_species))
+    {
+      // Triple-indexed vectors arent initialized as I expected
+      if (reacting_species.size() <= c)
         continue;
 
-      for (const auto c : index_range(reacting_species[s]))
-      {
-        params.set<std::vector<VariableName>>("vs") = {reacting_species[s][c]};
-        params.set<MaterialPropertyName>("reaction_rate_name") = reaction_coeffs[s][c];
-        params.set<Real>("coeff") = -1;
-
-        getProblem().addKernel(kernel_type,
-                               prefix() + var_name + "_reaction_" + var_name + "_" +
-                                   reacting_species[s][c],
-                               params);
-
-        // only if the other reacting species is a nonlinear variable
-        if (variableExists(reacting_species[s][c], false) && reacting_species[s][c] != var_name)
+      // only if the other reacting species is a nonlinear variable
+      // We skip the 'var_name' species because it's done right above
+      for (const auto & reactant : reacting_species[c])
+        if (solverVariableExists(reactant))
         {
-          params.set<NonlinearVariableName>("variable") = reacting_species[s][c];
-          params.set<std::vector<VariableName>>("vs") = {reacting_species[s][c]};
+          params.set<NonlinearVariableName>("variable") = reactant;
+          params.set<std::vector<VariableName>>("vs") = reacting_species[c];
+          params.set<MaterialPropertyName>("reaction_rate_name") = reaction_coeffs[c];
 
           params.set<Real>("coeff") = -1;
+          // Keep the index in the name to keep uniqueness
           getProblem().addKernel(kernel_type,
-                                 prefix() + var_name + "_coupled_reaction_" +
-                                     reacting_species[s][c] + "_" + var_name,
+                                 prefix() + reactant + ":coupled_reaction_of_" +
+                                     Moose::stringify(reacting_species[c]) + "_" +
+                                     std::to_string(c),
                                  params);
         }
+        else if (variableExists(reactant, false))
+          paramWarning("reacting_species",
+                       "Variable '" + reactant +
+                           "' is auxiliary, so its consumption cannot be tracked using kernels!");
+        else
+          paramError("reacting_species", "Reactant species '" + reactant + "' is not a variable");
 
-        // only if the target species is a nonlinear variable
-        if (product_species.size() && variableExists(product_species[s][c], false))
-        {
-          params.set<NonlinearVariableName>("variable") = product_species[s][c];
-          params.set<std::vector<VariableName>>("vs") = {reacting_species[s][c]};
+      // only if the target species is a nonlinear variable
+      if (product_species.size() > c)
+      {
+        for (const auto & product : product_species[c])
+          if (solverVariableExists(product))
+          {
+            params.set<NonlinearVariableName>("variable") = product;
+            params.set<std::vector<VariableName>>("vs") = reacting_species[c];
+            params.set<MaterialPropertyName>("reaction_rate_name") = reaction_coeffs[c];
 
-          params.set<Real>("coeff") = 1;
-          getProblem().addKernel(kernel_type,
-                                 prefix() + var_name + "_production_" + var_name + "_" +
-                                     reacting_species[s][c],
-                                 params);
-        }
+            params.set<Real>("coeff") = 1;
+            // Keep the index in the name to keep uniqueness
+            getProblem().addKernel(kernel_type,
+                                   prefix() + product + ":production_of_" + product + "_from_" +
+                                       Moose::stringify(reacting_species[c]) + "_" +
+                                       std::to_string(c),
+                                   params);
+          }
+          else if (variableExists(product, false))
+            paramWarning("product_species",
+                         "Variable '" + product +
+                             "' is auxiliary, so its production cannot be tracked using kernels!");
+          else
+            paramError("product_species", "Product species '" + product + "' is not a variable");
       }
     }
   }
