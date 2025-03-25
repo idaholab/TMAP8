054b77fa55568cf1e9e6fe2a00ee33070f96d5ed
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-03-24 19:19:12 -0600

    Use a different treatment to add the released term

--- include/nodal_kernels/ReleasingNodalKernel.h
+++ include/nodal_kernels/ReleasingNodalKernel.h
@@ -20,8 +20,18 @@ public:
 protected:
   Real computeQpResidual() override;
   Real computeQpJacobian() override;
+  Real computeQpOffDiagJacobian(unsigned int jvar) override;
 
+  /// Release coefficient
   const Real _alpha_r;
+  /// Energy from detrapping
   const Real _detrapping_energy;
+  /// Local temperature
   const VariableValue & _temperature;
+  /// Species concentration
+  const VariableValue & _v;
+  /// Index of the species variable
+  const unsigned int _v_index;
+  /// Whether the v variable is the kernel's variable parameter variable
+  const bool _v_is_u;
 };
