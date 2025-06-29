d079cdcd6cce07679d870de343b2c20d643118d7
Lee Kadz <leeabrahamthom.kadz@inl.gov>
2025-06-29 17:22:41 -0600

    typo in the sample size and add the flux on top

---
+++ test/tests/val-2g/comparison_val-2g.py
@@ -0,0 +1,185 @@
+import numpy as np
+import pandas as pd
+import os
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
+import glob
+import re
+
+# Changes working directory to script directory (for consistent MooseDocs usage)
+script_folder = os.path.dirname(__file__)
+os.chdir(script_folder)
+
+#===============================================================================
+# Load experimental data
+if "/tmap8/doc/" in script_folder.lower():  # if in documentation folder
+    csv_folder = "../../../../test/tests/val-2g/gold/val-2g_823K_1210Pa_out.csv"
+else:                                       # if in test folder
+    csv_folder = "./gold/val-2g_823K_1210Pa_out.csv"
+if "/tmap8/doc/" in script_folder.lower():  # if in documentation folder
+    data_folder = "../../../../test/tests/val-2g/gold/"
+else:                                       # if in test folder
+    data_folder = "./gold/"
+data_by_temperature = {}
+expt_data = pd.read_csv(csv_folder)
+TMAP8_time = expt_data['time']
+concentration_ratio = expt_data['concentration_ratio_tritium']
+concentration_ratio_tritium = expt_data['concentration_ratio_tritium']
+sieverts_ratio_tritium = expt_data['sieverts_ratio_tritium']
+average_flux_left = - expt_data['average_flux_left']
+average_flux_Ni_FLiBe_interface = expt_data['average_flux_Ni_FLiBe_interface']
+average_flux_right = expt_data['average_flux_right']
+
+#===============================================================================
+# General parameters
+
+R = 8.31446261815324 # J/mol/K ideal gas constant from PhysicalConstants.h
+temperature = 823 # K
+n_henry = 1 # Henry's law
+
+#===============================================================================
+# Flux conservation
+start_time = 2000 # characteristic time for convergence
+flux_conservation = average_flux_left/average_flux_Ni_FLiBe_interface
+fig = plt.figure(figsize=[6.5,5.5])
+gs = gridspec.GridSpec(1,1)
+ax = fig.add_subplot(gs[0])
+ratio = [1] * len(TMAP8_time[1:])
+ax.plot(TMAP8_time[start_time:], flux_conservation[start_time:], label=r"Concentration Ratio (TMAP8)", color='tab:blue', linestyle='-')
+ax.set_xlim(TMAP8_time[start_time],TMAP8_time[start_time:].max())
+ax.set_xlabel('Time (s)')
+ax.set_ylabel(r"Flux conservation")
+ax.legend(loc="best")
+ax.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
+RMSE = np.sqrt(np.mean((flux_conservation[start_time:]-ratio[start_time-1:])**2))
+RMSPE = RMSE*100/np.mean(ratio[start_time-1:])
+print("RMSPE for flux conservation: ", RMSPE)
+x_pos = TMAP8_time[start_time:].max()*0.65
+y_pos = flux_conservation[start_time].max()
+ax.text(x_pos, y_pos, 'RMSPE = %.3f ' % RMSPE + '%', fontweight='bold')
+fig.savefig('val-2g_flux_conservation.png', bbox_inches='tight', dpi=300)
+
+#===============================================================================
+# Sieverts' law conservation enclosure-Ni
+start_time = 100 # characteristic time for convergence
+K_s_Ni_prefactor = 564e-3 # mol/m^3/Pa^0.5
+K_s_Ni_energy = 15.8e3 # J/mol
+K_s_Ni = K_s_Ni_prefactor * np.exp(- K_s_Ni_energy / (R*temperature)) # mol/m^3/Pa^0.5
+fig = plt.figure(figsize=[6.5,5.5])
+gs = gridspec.GridSpec(1,1)
+ax = fig.add_subplot(gs[0])
+solubility_ratio = [K_s_Ni] * len(TMAP8_time[1:])
+ax.plot(TMAP8_time[start_time:], sieverts_ratio_tritium[start_time:], label=r"Concentration Ratio (TMAP8)", color='tab:blue', linestyle='-')
+ax.set_xlim(TMAP8_time[start_time],TMAP8_time[start_time:].max())
+ax.set_xlabel('Time (s)')
+ax.set_ylabel(r"Concentrations ratio $C_{\text{Ni}} / \sqrt{P_{T_2}}$")
+ax.legend(loc="best")
+ax.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
+RMSE = np.sqrt(np.mean((sieverts_ratio_tritium[start_time:]-solubility_ratio[start_time-1:])**2))
+RMSPE = RMSE*100/np.mean(solubility_ratio[start_time-1:])
+print("RMSPE for sorption law enclosure-Ni: ", RMSPE)
+x_pos = TMAP8_time[start_time:].max() / 50
+y_pos = sieverts_ratio_tritium[start_time].max()*1.005
+ax.text(x_pos, y_pos, 'RMSPE = %.3f ' % RMSPE + '%', fontweight='bold')
+fig.savefig('val-2g_concentration_ratio_enclosure-Ni.png', bbox_inches='tight', dpi=300)
+
+#===============================================================================
+# Henry's law conservation Ni-FLiBe
+start_time = 500 # characteristic time for convergence
+K_s_FLiBe_prefactor = 7.9e-2 # mol/m^3/Pa
+K_s_FLiBe_energy = 35e3 # J/mol
+K_s_FLiBe = K_s_FLiBe_prefactor * np.exp(- K_s_FLiBe_energy / (R*temperature)) # mol/m^3/Pa
+fig = plt.figure(figsize=[6.5,5.5])
+gs = gridspec.GridSpec(1,1)
+ax = fig.add_subplot(gs[0])
+solubility_ratio = [K_s_FLiBe*(R*temperature)**n_henry] * len(TMAP8_time[1:])
+ax.ticklabel_format(useOffset=False, style='plain', axis='y')
+ax.get_yaxis().get_offset_text().set_visible(False)
+ax.plot(TMAP8_time[start_time:], concentration_ratio_tritium[start_time:], label=r"Concentration Ratio (TMAP8)", color='tab:blue', linestyle='-')
+ax.set_xlim(TMAP8_time[start_time],TMAP8_time[start_time:].max())
+ax.set_xlabel('Time (s)')
+ax.set_ylabel(r"Concentrations ratio $C_{\text{Ni}} / C_{\text{FLiBe}}$")
+ax.legend(loc="best")
+ax.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
+RMSE = np.sqrt(np.mean((concentration_ratio_tritium[start_time:]-solubility_ratio[start_time-1:])**2))
+RMSPE = RMSE*100/np.mean(solubility_ratio[start_time-1:])
+print("RMSPE for sorption law Ni-FLiBe: ", RMSPE)
+x_pos = TMAP8_time[start_time:].max()/10
+y_pos = concentration_ratio_tritium[start_time:].max()
+ax.text(x_pos, y_pos, 'RMSPE = %.3f ' % RMSPE + '%', fontweight='bold')
+fig.savefig('val-2g_concentration_ratio_Ni-FLiBe.png', bbox_inches='tight', dpi=300)
+
+#===============================================================================
+# Tritium permeation flux as a function of tritium partial pressure
+# for a range of temperatures and initial pressures
+
+fig = plt.figure(figsize=[6.5, 5.5])
+gs = gridspec.GridSpec(1, 1)
+ax = fig.add_subplot(gs[0])
+
+for filename in os.listdir(data_folder):
+    if filename.endswith(".csv") and filename.startswith("val-2g_"):
+        parts = filename.replace(".csv", "").split("_")
+        if len(parts) == 4 and parts[1].endswith("K") and parts[2].endswith("Pa"):
+            try:
+                T = int(parts[1][:-1])  # Remove 'K'
+                P = int(parts[2][:-2])  # Remove 'Pa'
+                df = pd.read_csv(os.path.join(data_folder, filename))
+                average_flux = df["average_flux_right"].iloc[-1]
+                data_by_temperature.setdefault(T, []).append((P, average_flux))
+            except Exception as e:
+                print(f"Skipping {filename}: {e}")
+
+# Define consistent color and marker maps based on temperature in Celsius
+temperatures_C = [550, 600, 650, 700]
+temperature_color_map = {T: f"C{i}" for i, T in enumerate(temperatures_C)}
+temperature_marker_map = {550: 'o', 600: 's', 650: 'D', 700: '^'}
+
+# Experimental data
+exp_data = pd.DataFrame({
+    "pressure": [170, 316, 316, 538, 538, 538, 1210, 1210, 1210, 1210],
+    "flux": np.array([
+        1.0684039087947879,
+        1.0423452768729637,
+        2.0325732899022793,
+        1.1465798045602602,
+        1.745928338762214,
+        2.866449511400651,
+        3.6351791530944624,
+        2.5146579804560254,
+        1.7068403908794778,
+        1.1596091205211714
+    ]) * 1e-7,
+    "temperature": [700, 650, 700, 600, 650, 700, 700, 650, 600, 550]
+})
+
+for T_C in reversed(sorted(exp_data["temperature"].unique())):
+    subset = exp_data[exp_data["temperature"] == T_C]
+    ax.scatter(
+        subset["pressure"],
+        subset["flux"],
+        marker=temperature_marker_map[T_C],
+        facecolors='none',
+        edgecolors=temperature_color_map[T_C],
+        label=f"{T_C} °C exp"
+    )
+
+# Plot TMAP8 data
+for T in reversed(sorted(data_by_temperature.keys())):
+    T_C = T - 273
+    values = sorted(data_by_temperature[T])
+    pressures, fluxes = zip(*values)
+    ax.scatter(
+        pressures,
+        fluxes,
+        marker=temperature_marker_map.get(T_C, 'x'),
+        color=temperature_color_map.get(T_C, 'k'),
+        label=f"{T_C} °C (TMAP8)"
+    )
+
+# Styling
+ax.set_xlabel(r"$T_2$ partial pressure (Pa)")
+ax.set_ylabel(r"Tritium molar flux (mol/m$^2$/s)")
+ax.legend(loc="best")
+ax.grid(which='major', color='0.65', linestyle='--', alpha=0.3)
+fig.savefig('val-2g_tritium_flux.png', bbox_inches='tight', dpi=300)
---
+++ test/tests/val-2g/val-2g.i
@@ -0,0 +1,267 @@
+# Global parameters
+R = '${units 8.31446261815324 J/mol/K}' # ideal gas constant from PhysicalConstants.h
+simulation_time = '${units 2e5 s}'
+temperature = '${units 823 K}'
+
+# Sorption law parameters
+n_Henry = 1 # Henry's law
+n_Sieverts = 0.5 # Sieverts' law
+unit_scale = 1
+unit_scale_neighbor = 1
+
+# Materials properties
+## diffusivity of tritium in FLiBe
+D_FLiBe_prefactor = '${units 9.3e-7 m^2/s}'
+D_FLiBe_energy = '${units 42e3 J/mol}'
+D_FLiBe = '${units ${fparse D_FLiBe_prefactor * exp(- D_FLiBe_energy / (R*temperature))} m^2/s}'
+## diffusivity of tritium in nickel
+D_Ni_prefactor = '${units 7e-7 m^2/s}'
+D_Ni_energy = '${units 39.5e3 J/mol}'
+D_Ni = '${units ${fparse D_Ni_prefactor * exp(- D_Ni_energy / (R*temperature))} m^2/s}'
+## Sieverts' law solubility for tritium in nickel
+K_s_Ni_prefactor = '${units 564e-3 mol/m^3/Pa^0.5}'
+K_s_Ni_energy = '${units 15.8e3 J/mol}'
+## Henry's law solubility for tritium in FLiBe
+K_s_FLiBe_prefactor = '${units 7.9e-2 mol/m^3/Pa}'
+K_s_FLiBe_energy = '${units 35e3 J/mol}'
+
+# Initial conditions
+initial_pressure = '${units 1210 Pa}' # input pressure
+initial_concentration_Ni = '${units 1e-12 mol/m^3}'
+initial_concentration_FLiBe = '${units 1e-12 mol/m^3}'
+
+# Geometry and mesh
+length_Ni = '${units 2 mm -> m}' # Ni membrane thickness
+num_nodes_Ni = 10
+length_FLiBe = '${units 8.1 mm -> m}' # FLiBe membrane thickness
+num_nodes_FLiBe = 100
+
+[Mesh]
+  [cmg]
+    type = CartesianMeshGenerator
+    dim = 1
+    dx = '${length_Ni} ${length_FLiBe}'
+    ix = '${num_nodes_Ni} ${num_nodes_FLiBe}'
+    subdomain_id = '1 2'
+  []
+  [interface_Ni_FLiBe]
+    type = SideSetsBetweenSubdomainsGenerator
+    input = cmg
+    primary_block = '1'
+    paired_block = '2'
+    new_boundary = 'interface_Ni_FLiBe'
+  []
+  [interface_Ni_FLiBe_other_direction]
+    type = SideSetsBetweenSubdomainsGenerator
+    input = interface_Ni_FLiBe
+    primary_block = '2'
+    paired_block = '1'
+    new_boundary = 'interface_Ni_FLiBe_other_direction'
+  []
+[]
+
+[Variables]
+  [tritium_concentration_Ni]
+    initial_condition = ${initial_concentration_Ni} # mol/m^3
+    block = 1
+  []
+  [tritium_concentration_FLiBe]
+    initial_condition = ${initial_concentration_FLiBe} # mol/m^3
+    block = 2
+  []
+[]
+
+[AuxVariables]
+  [enclosure_pressure]
+    family = SCALAR
+    initial_condition = ${initial_pressure}
+  []
+[]
+
+[Kernels]
+  # Diffusion in Ni membrane
+  [diffusion_Ni]
+    type = ADMatDiffusion
+    variable = 'tritium_concentration_Ni'
+    diffusivity = ${D_Ni}
+    block = 1
+  []
+  [time_diff_Ni]
+    type = TimeDerivative
+    variable = 'tritium_concentration_Ni'
+    block = 1
+  []
+  # Diffusion in FLiBe
+  [diffusion_FLiBe]
+    type = ADMatDiffusion
+    variable = 'tritium_concentration_FLiBe'
+    diffusivity = ${D_FLiBe}
+    block = 2
+  []
+  [time_diff_FLiBe]
+    type = TimeDerivative
+    variable = 'tritium_concentration_FLiBe'
+    block = 2
+  []
+[]
+
+[BCs]
+  [left_concentration]
+    type = EquilibriumBC
+    Ko = ${K_s_Ni_prefactor}
+    activation_energy = ${K_s_Ni_energy}
+    boundary = 'left'
+    enclosure_var = 'enclosure_pressure'
+    temperature = ${temperature}
+    variable = 'tritium_concentration_Ni'
+    p = ${n_Sieverts}
+  []
+  [right_concentration]
+    type = ADDirichletBC
+    boundary = 'right'
+    variable = 'tritium_concentration_FLiBe'
+    value = 0.0
+  []
+[]
+
+[InterfaceKernels]
+  [interface_sorption_Ni_FLiBe]
+    type = InterfaceSorption
+    K0 = ${K_s_FLiBe_prefactor}
+    Ea = ${K_s_FLiBe_energy}
+    n_sorption = ${n_Henry}
+    diffusivity = ${D_FLiBe}
+    unit_scale = ${unit_scale}
+    unit_scale_neighbor = ${unit_scale_neighbor}
+    temperature = ${temperature}
+    variable = 'tritium_concentration_Ni'
+    neighbor_var = 'tritium_concentration_FLiBe'
+    sorption_penalty = 1e1
+    boundary = 'interface_Ni_FLiBe'
+  []
+[]
+
+[Functions]
+  [max_dt_size_function]
+    type = ParsedFunction
+    expression = 'if(t<${fparse 8500}, ${fparse 1e2},
+                  if(t<${fparse 37000}, ${fparse 1e1},
+                  if(t<${fparse 38500}, ${fparse 1e0}, ${fparse 1e1})))'
+  []
+[]
+
+[Postprocessors]
+  [average_flux_left]
+    type = SideDiffusiveFluxAverage
+    variable = 'tritium_concentration_Ni'
+    boundary = 'left'
+    diffusivity = ${D_Ni}
+  []
+  [average_flux_Ni_FLiBe_interface]
+    type = SideDiffusiveFluxAverage
+    variable = 'tritium_concentration_Ni'
+    boundary = 'interface_Ni_FLiBe'
+    diffusivity = ${D_Ni}
+  []
+  [average_flux_right]
+    type = SideDiffusiveFluxAverage
+    variable = 'tritium_concentration_FLiBe'
+    boundary = 'right'
+    diffusivity = ${D_FLiBe}
+  []
+  [Ni_left]
+    type = SideAverageValue
+    boundary = 'left'
+    variable = 'tritium_concentration_Ni'
+    execute_on = 'initial timestep_end'
+  []
+  [Ni_interface]
+    type = SideAverageValue
+    boundary = 'interface_Ni_FLiBe'
+    variable = 'tritium_concentration_Ni'
+    execute_on = 'initial timestep_end'
+  []
+  [FLiBe_interface]
+    type = SideAverageValue
+    boundary = 'interface_Ni_FLiBe_other_direction'
+    variable = 'tritium_concentration_FLiBe'
+    execute_on = 'initial timestep_end'
+  []
+  [concentration_ratio_tritium]
+    type = ParsedPostprocessor
+    expression = 'Ni_interface / (FLiBe_interface^${n_Henry})'
+    pp_names = 'Ni_interface FLiBe_interface'
+    execute_on = 'initial timestep_end'
+  []
+  [concentration_tritium_Ni_inventory]
+    type = ElementIntegralVariablePostprocessor
+    variable = 'tritium_concentration_Ni'
+    block = 1
+    execute_on = 'initial timestep_end'
+  []
+  [concentration_tritium_FLiBe_inventory]
+    type = ElementIntegralVariablePostprocessor
+    variable = 'tritium_concentration_FLiBe'
+    block = 2
+    execute_on = 'initial timestep_end'
+  []
+  [mass_conservation]
+    type = LinearCombinationPostprocessor
+    pp_names = 'concentration_tritium_Ni_inventory concentration_tritium_FLiBe_inventory'
+    pp_coefs = '1 1'
+    execute_on = 'initial timestep_end'
+  []
+  [sieverts_ratio_tritium]
+    type = ParsedPostprocessor
+    expression = 'Ni_left / (${initial_pressure})^${n_Sieverts}'
+    pp_names = 'Ni_left'
+    execute_on = 'initial timestep_end'
+    outputs = 'csv console'
+  []
+  [max_time_step_size]
+    type = FunctionValuePostprocessor
+    function = 'max_dt_size_function'
+    execute_on = 'initial nonlinear linear timestep_end'
+    outputs = none
+  []
+[]
+
+[Preconditioning]
+  [SMP]
+    type = SMP
+    full = true
+  []
+[]
+
+[Executioner]
+  type = Transient
+  end_time = ${simulation_time}
+  nl_max_its = 9
+  l_max_its = 30
+  scheme = 'bdf2'
+  solve_type = 'PJFNK'
+  petsc_options_iname = '-pc_type'
+  petsc_options_value = 'lu'
+  nl_abs_tol = 1e-11
+  [TimeStepper]
+    type = IterationAdaptiveDT
+    dt = 1e-6
+    optimal_iterations = 7
+    iteration_window = 1
+    growth_factor = 1.1
+    cutback_factor = 0.9
+    cutback_factor_at_failure = 0.9
+    timestep_limiting_postprocessor = max_time_step_size
+  []
+[]
+
+[Outputs]
+  file_base = 'val-2g_out'
+  [csv]
+    type = CSV
+  []
+  [exodus]
+    type = Exodus
+    time_step_interval = 200
+  []
+[]
