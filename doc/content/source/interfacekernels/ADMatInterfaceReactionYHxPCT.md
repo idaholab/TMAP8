6a315c7cc1abec8487dd9a9694617e134184c0af
Bowers <boweag@br175.ib>
2025-09-15 09:58:54 -0600

    Ref#262,Implementing low pressure, additional yttrium documentation, test files for low pressure, and fixed trailing whitespace

--- doc/content/source/interfacekernels/ADMatInterfaceReactionYHxPCT.md
+++ doc/content/source/interfacekernels/ADMatInterfaceReactionYHxPCT.md
@@ -16,13 +16,19 @@ $K_b$ and $K_f$ are the backward and forward surface reaction rate in 1/s, respe
 $f_{at}(T,P)$ is the composition in atomic fraction of H in YHx given a gas temperature $T$ and gas pressure $P$,
 and $\rho$ is the yttrium atomic density in mol/m$^3$.
 
-[YHx_PCT_fit_2D] shows the data used in this interface kernel. The experimental data originates from [!cite](Lundin_1962), and the fit is from [!cite](Matthews2021SWIFT).
+[YHx_PCT_fit_2D_HighPressure] and [YHx_PCT_fit_2D_LowPressure] shows the data used in this interface kernel. The experimental data originates from [!cite](Lundin_1962), and [YHx_PCT_fit_2D_HighPressure] fit is from [!cite](Matthews2021SWIFT), where as [YHx_PCT_fit_2D_LowPressure] is a newly fitted curve.
 
 !media comparison_YHx_PCT.py
-       image_name=YHx_PCT_fit_2D.png
+       image_name=YHx_PCT_fit_2D_HighPressure.png
        style=width:80%;margin-bottom:2%;margin-left:auto;margin-right:auto
-       id=YHx_PCT_fit_2D
+       id=YHx_PCT_fit_2D_HighPressure
        caption=PCT data for YHx from [!cite](Lundin_1962) with fits from [!cite](Matthews2021SWIFT) implemented in TMAP8, and test results.
+
+!media comparison_YHx_PCT.py
+       image_name=YHx_PCT_fit_2D_LowPressure.png
+       style=width:80%;margin-bottom:2%;margin-left:auto;margin-right:auto
+       id=YHx_PCT_fit_2D_LowPressure
+       caption=PCT data for YHx from [!cite](Lundin_1962) implemented in TMAP8, and test results.
 
 The plateau representing phase transition is captured as [!citep](Matthews2021SWIFT):
 \begin{equation} \label{eq:pressure_plateau}
@@ -37,16 +43,15 @@ This fit is shown in [YHx_PCT_plateau_pressure_fit].
        id=YHx_PCT_plateau_pressure_fit
        caption=Fit phase transition region as pressure as a function of temperature.
 
-The model currently only captures the high pressure region of the data, i.e., for pressure and temperatures above the plateau.
-In this region, the atomic fraction $f_{at}$ is determined as [!citep](Matthews2021SWIFT):
-\begin{equation} \label{eq:atomic_fraction}
+In the high pressure region, the atomic fraction $f_{at}$ is determined as [!citep](Matthews2021SWIFT):
+\begin{equation}
 f_{at}(T,P) = 2-\left[1+\exp(21.6 -0.0225 T + (-0.0445 + 7.18 \times 10^{-4} T) (\log\left(P - P_{lim}(T)\right)))\right]^{-1},
 \end{equation}
-where $P$ is the hydrogen partial pressure in Pa. This fit is plotted in [YHx_PCT_fit_2D].
-
-The [!param](/InterfaceKernels/ADMatInterfaceReactionYHxPCT/silence_warnings) option can be used to dictate how TMAP8 reacts when the pressure gets out of bounds.
-If `silence_warnings = false`, which is the default behavior, then TMAP8 will print a warning stating that the pressure and temperature are outside the bounds of the atomic fraction correlation.
-If `silence_warnings = true`, then TMAP8 will let the simulation continue without issuing any warnings.
+where $P$ is the hydrogen partial pressure in Pa. This fit is plotted in [YHx_PCT_fit_2D_HighPressure]. While in the low pressure region, the atomic fraction $f_{at}$ is determined as:
+\begin{equation}
+f_{at}(T,P) = 0.5-\left[0.001+\exp(-86.835 + 0.095078 T + (0.95502 - 4.2038 \times 10^{-3} T) (\log\left(P_{lim}(T) - P\right)))\right]^{-1},
+\end{equation}
+This fit is plotted in [YHx_PCT_fit_2D_LowPressure].
 
 ## Test
 
@@ -59,7 +64,7 @@ To model the interface, the input file employs the [InterfaceDiffusion.md] objec
 \end{equation}
 where $\rho$ is the yttrium atomic density.
 
-The results of the test for ($T$, $P$) = (1173.15 K, $1 \times 10^{3}$ Pa), (1173.15 K, $1 \times 10^{4}$ Pa), (1173.15 K, $5 \times 10^{4}$ Pa), and(1473.15 K, $5 \times 10^{4}$ Pa) are shown in [YHx_PCT_fit_2D] and show good agreement with [eq:atomic_fraction].
+The results of the high pressure test for ($T$, $P$) = (1173.15 K, $1 \times 10^{3}$ Pa), (1173.15 K, $1 \times 10^{4}$ Pa), (1173.15 K, $5 \times 10^{4}$ Pa), and(1473.15 K, $5 \times 10^{4}$ Pa) are shown in [YHx_PCT_fit_2D_HighPressure]. While the low pressure test for ($T$, $P$) = (1473.15 K, $3 \times 10^{3}$ Pa), (1273.15 K, $3 \times 10^{2}$ Pa), (1573.15 K, $5 \times 10^{3}$ Pa), and(1573.15 K, $6 \times 10^{2}$ Pa) are shown in [YHx_PCT_fit_2D_LowPressure].
 
 ## Example Input File Syntax
 
--- src/interfacekernels/ADMatInterfaceReactionYHxPCT.C
+++ src/interfacekernels/ADMatInterfaceReactionYHxPCT.C
@@ -1,10 +1,10 @@
-/************************************************************/
+/*************************************************************/
 /*                DO NOT MODIFY THIS HEADER                 */
 /*   TMAP8: Tritium Migration Analysis Program, Version 8   */
 /*                                                          */
 /*   Copyright 2021 - 2025 Battelle Energy Alliance, LLC    */
 /*                   ALL RIGHTS RESERVED                    */
-/************************************************************/
+/*************************************************************/
 
 #include "ADMatInterfaceReactionYHxPCT.h"
 
@@ -29,8 +29,6 @@ ADMatInterfaceReactionYHxPCT::validParams()
       "forward_rate", "kf", "Forward reaction rate coefficient (1/s).");
   params.addParam<MaterialPropertyName>(
       "backward_rate", "kb", "Backward reaction rate coefficient (1/s).");
-  params.addParam<bool>(
-      "silence_warnings", false, "Whether to silence correlation out of bound warnings");
   return params;
 }
 
@@ -39,22 +37,17 @@ ADMatInterfaceReactionYHxPCT::ADMatInterfaceReactionYHxPCT(const InputParameters
     _neighbor_temperature(this->template coupledGenericValue<true>("neighbor_temperature")),
     _density(getADMaterialProperty<Real>("density")),
     _kf(getADMaterialProperty<Real>("forward_rate")),
-    _kb(getNeighborADMaterialProperty<Real>("backward_rate")),
-    _silence_warnings(getParam<bool>("silence_warnings"))
+    _kb(getNeighborADMaterialProperty<Real>("backward_rate"))
 {
 }
 
 ADReal
 ADMatInterfaceReactionYHxPCT::computeQpResidual(Moose::DGResidualType type)
 {
-  using std::exp;
-  using std::log;
-  using std::max;
-  using std::pow;
   ADReal r = 0;
-
+  const Real tolerance = 10;
   // Calculate the equilibrium concentration value based on PCT curve
-  // (/2 because two atoms for a molecule) (pressure in Pa)
+  // (/2 because two atoms for a molecule) (pressure in Pa)V3_projects/TMAP8/src/bcs
   auto neighbor_pressure =
       PhysicalConstants::ideal_gas_constant * _neighbor_temperature[_qp] * _neighbor_value[_qp] / 2;
 
@@ -62,21 +55,54 @@ ADMatInterfaceReactionYHxPCT::computeQpResidual(Moose::DGResidualType type)
   auto limit_pressure = exp(-26.1 + 3.88e-2 * _neighbor_temperature[_qp] -
                             9.7e-6 * Utility::pow<2>(_neighbor_temperature[_qp]));
 
-  // return warning if the PCT curves is used out of bounds (pressure in Pa)
-  if (!_silence_warnings && ((neighbor_pressure < limit_pressure) || (neighbor_pressure > 1.e6)))
-    mooseDoOnce(mooseWarning("In YHxPCT: pressure ",
-                             neighbor_pressure,
-                             "Pa and temperature ",
-                             _neighbor_temperature[_qp],
-                             "K are outside the bounds of the atomic fraction correlation. See "
-                             "documentation for YHxPCT material."));
-
-  // Calculate the atomic fraction based on the PCT curve
   auto atomic_fraction =
-      2. - pow(1. + exp(21.6 - 0.0225 * _neighbor_temperature[_qp] +
-                        (-0.0445 + 7.18e-4 * _neighbor_temperature[_qp]) *
-                            (log(max(neighbor_pressure - limit_pressure, 1e-10)))),
-               -1);
+      0.5 - pow(0.001 + exp(-8.6835e01 + 9.5078e-02 * _neighbor_temperature[_qp] +
+                            (9.5502e-01 - 4.2038e-3 * _neighbor_temperature[_qp]) *
+                                (log(max(limit_pressure - neighbor_pressure, 1e-10)))),
+                -1);
+
+  if (neighbor_pressure - limit_pressure > 0)
+  {
+    // we are in the High pressure region
+    if (abs(neighbor_pressure - limit_pressure) < tolerance)
+    {
+      // Low Pressure Maximum
+      atomic_fraction = 0.5;
+    }
+    else
+    {
+
+      // High Pressure
+      // Calculate the atomic fraction based on the PCT curve
+      atomic_fraction =
+          2. - pow(1. + exp(21.6 - 0.0225 * _neighbor_temperature[_qp] +
+                            (-0.0445 + 7.18e-4 * _neighbor_temperature[_qp]) *
+                                (log(max(neighbor_pressure - limit_pressure, 1e-10)))),
+                   -1);
+    }
+  }
+
+  else if (neighbor_pressure - limit_pressure < 0)
+  {
+    // we are in the Low pressure region
+    if (abs(neighbor_pressure - limit_pressure) < tolerance)
+    {
+      // High Pressure minimum
+      atomic_fraction = 1.0;
+    }
+    else
+    {
+
+      // High Pressure
+      // Low Pressure
+      // Calculate the atomic fraction based on the PCT curve
+      atomic_fraction =
+          0.5 - pow(0.001 + exp(-8.6835e01 + 9.5078e-02 * _neighbor_temperature[_qp] +
+                                (9.5502e-01 - 4.2038e-3 * _neighbor_temperature[_qp]) *
+                                    (log(max(limit_pressure - neighbor_pressure, 1e-10)))),
+                    -1);
+    }
+  }
 
   // Convert to concentration
   auto _surface_equilibrium_concentration = atomic_fraction * _density[_qp];
@@ -98,4 +124,59 @@ ADMatInterfaceReactionYHxPCT::computeQpResidual(Moose::DGResidualType type)
       break;
   }
   return r;
+  // OLD PC WORK THAT EXCLUSIVELY DOES HIGH PRESSURE REGIMES
+  /*
+    using std::exp;
+    using std::log;
+    using std::max;
+    using std::pow;
+    ADReal r = 0;
+
+    // Calculate the equilibrium concentration value based on PCT curve
+    // (/2 because two atoms for a molecule) (pressure in Pa)
+    auto neighbor_pressure =
+        PhysicalConstants::ideal_gas_constant * _neighbor_temperature[_qp] * _neighbor_value[_qp] /
+    2;
+
+    // Calculate the value of the pressures for the phase transition plateau (pressure in Pa)
+    auto limit_pressure = exp(-26.1 + 3.88e-2 * _neighbor_temperature[_qp] -
+                              9.7e-6 * Utility::pow<2>(_neighbor_temperature[_qp]));
+
+    // return warning if the PCT curves is used out of bounds (pressure in Pa)
+    if (!_silence_warnings && ((neighbor_pressure < limit_pressure) || (neighbor_pressure > 1.e6)))
+      mooseDoOnce(mooseWarning("In YHxPCT: pressure ",
+                               neighbor_pressure,
+                               "Pa and temperature ",
+                               _neighbor_temperature[_qp],
+                               "K are outside the bounds of the atomic fraction correlation. See "
+                               "documentation for YHxPCT material."));
+
+    // Calculate the atomic fraction based on the PCT curve
+    auto atomic_fraction =
+        2. - pow(1. + exp(21.6 - 0.0225 * _neighbor_temperature[_qp] +
+                          (-0.0445 + 7.18e-4 * _neighbor_temperature[_qp]) *
+                              (log(max(neighbor_pressure - limit_pressure, 1e-10)))),
+                 -1);
+
+    // Convert to concentration
+    auto _surface_equilibrium_concentration = atomic_fraction * _density[_qp];
+
+    switch (type)
+    {
+      // Move all the terms to the LHS to get residual, for primary domain
+      // Residual = kf*u - kb*v
+      // Weak form for primary domain is: (test, kf*u - kb*v)
+      case Moose::Element:
+        r = _test[_i][_qp] * (_kf[_qp] * _u[_qp] - _kb[_qp] * _surface_equilibrium_concentration);
+        break;
+
+      // Similarly, weak form for secondary domain is: -(test, kf*u - kb*v),
+      // flip the sign because the direction is opposite.
+      case Moose::Neighbor:
+        r = -_test_neighbor[_i][_qp] *
+            (_kf[_qp] * _u[_qp] - _kb[_qp] * _surface_equilibrium_concentration);
+        break;
+    }
+    return r;
+    */
 }
