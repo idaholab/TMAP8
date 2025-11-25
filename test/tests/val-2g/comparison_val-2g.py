9d19efd82889ed728d5ab9d6df2dff176e3af244
Casey Icenhour <casey.icenhour@inl.gov>
2025-11-25 15:28:52 -0700

    Add TMAP4 run data to plot comparison script

--- test/tests/val-2g/comparison_val-2g.py
+++ test/tests/val-2g/comparison_val-2g.py
@@ -9,9 +9,9 @@ import os
 script_folder = os.path.dirname(__file__)
 os.chdir(script_folder)
 
-# Read simulation data
+# Read TMAP8 simulation data
 if "/tmap8/doc/" in script_folder.lower():     # if in documentation folder
-    csv_folder = "../../../../test/tests/val-2d/val-2g_673_out.csv"
+    csv_folder = "../../../../test/tests/val-2g/val-2g_673_out.csv"
 else:                                  # if in test folder
     csv_folder = "./val-2g_673_out.csv"
 simulation_673_data = pd.read_csv(csv_folder)
@@ -19,7 +19,7 @@ simulation_time_673 = simulation_673_data['time']
 simulation_flux_left_673 = simulation_673_data['scaled_flux_surface_left']
 
 if "/tmap8/doc/" in script_folder.lower():     # if in documentation folder
-    csv_folder = "../../../../test/tests/val-2d/val-2g_873_out.csv"
+    csv_folder = "../../../../test/tests/val-2g/val-2g_873_out.csv"
 else:                                  # if in test folder
     csv_folder = "./val-2g_873_out.csv"
 simulation_873_data = pd.read_csv(csv_folder)
@@ -27,13 +27,24 @@ simulation_time_873 = simulation_873_data['time']
 simulation_flux_left_873 = simulation_873_data['scaled_flux_surface_left']
 
 if "/tmap8/doc/" in script_folder.lower():     # if in documentation folder
-    csv_folder = "../../../../test/tests/val-2d/val-2g_973_out.csv"
+    csv_folder = "../../../../test/tests/val-2g/val-2g_973_out.csv"
 else:                                  # if in test folder
     csv_folder = "./val-2g_973_out.csv"
 simulation_973_data = pd.read_csv(csv_folder)
 simulation_time_973 = simulation_973_data['time']
 simulation_flux_left_973 = simulation_973_data['scaled_flux_surface_left']
 
+# Read TMAP4 simulation data
+if "/tmap8/doc/" in script_folder.lower():     # if in documentation folder
+    csv_folder = "../../../../test/tests/val-2g/tmap4_data.csv"
+else:                                  # if in test folder
+    csv_folder = "./tmap4_data.csv"
+simulation_TMAP4_data = pd.read_csv(csv_folder)
+simulation_time_TMAP4 = simulation_TMAP4_data['time']
+simulation_temperature_TMAP4 = simulation_TMAP4_data['temperature']
+simulation_flux_TMAP4_673 = simulation_TMAP4_data['TMAP - 400C']
+simulation_flux_TMAP4_873 = simulation_TMAP4_data['TMAP - 600C']
+simulation_flux_TMAP4_973 = simulation_TMAP4_data['TMAP - 700C']
 
 # Read experiment data
 # if "/tmap8/doc/" in script_folder.lower():     # if in documentation folder
@@ -51,9 +62,16 @@ fig = plt.figure(figsize=[6.5, 5.5])
 gs = gridspec.GridSpec(1, 1)
 ax = fig.add_subplot(gs[0])
 
+# TMAP8
 ax.semilogy(simulation_time_673-12000, simulation_flux_left_673, linestyle='-', label=r"673 K", c='blue', linewidth=3.0)
 ax.semilogy(simulation_time_873-12000, simulation_flux_left_873, linestyle='-', label=r"873 K", c='red', linewidth=3.0)
 ax.semilogy(simulation_time_973-12000, simulation_flux_left_973, linestyle='-', label=r"973 K", c='green', linewidth=3.0)
+
+# TMAP4
+ax.semilogy(simulation_time_TMAP4, simulation_flux_TMAP4_673, linestyle='--', label=r"TMAP4 - 673 K", c='blue', linewidth=3.0)
+ax.semilogy(simulation_time_TMAP4, simulation_flux_TMAP4_873, linestyle='--', label=r"TMAP4 - 873 K", c='red', linewidth=3.0)
+ax.semilogy(simulation_time_TMAP4, simulation_flux_TMAP4_973, linestyle='--', label=r"TMAP4 - 973 K", c='green', linewidth=3.0)
+
 
 # Font sizes for labels and axes
 SMALL_SIZE = 12
